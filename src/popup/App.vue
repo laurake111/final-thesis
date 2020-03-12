be978458b4d87a8b7a5527ee0939a3fec334a5b6
lleisb <lleisb@ttu.ee>
2020-03-12 12:46:20 +0200

    initial commit with extension popup

---
+++ src/popup/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Speech recogniser for Gmail</h5>
+                        <p class="card-text">Google extension that supports writing emails on Gmail with speech recognition</p>
+                        <a type="button" class="btn btn-outline-danger btn-sm" v-on:click="buttonSearch">Write email now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    const browser = require("webextension-polyfill");
+    export default {
+        data() {
+            return {
+                keyword: "",
+            };
+        },
+        methods: {
+            buttonSearch() {
+                window.open("https://mail.google.com");
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        width: 300px;
+        text-align: center;
+        padding: 10px;
+    }
+
+    .card {
+        border: none;
+        p {
+            font-size: 14px;
+        }
+    }
+
+    .button{
+        transition-duration: 0.4s;
+        :hover {
+            color: white;
+        }
+    }
+
+</style>
